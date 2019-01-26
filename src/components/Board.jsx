5af561cdade33ca105b0f05b5a81e17f7b9b2204
serpient <littlegangwolf@gmail.com>
2019-01-25 23:53:17 -0800

    add coin styling

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,11 +7,17 @@ const Board = ({ boardData }) => {
     let coinArray = [];
     for (var value of mapObject.values()) {
       if (value === User.COMPUTER) {
-        coinArray.push(<div className='card card--computer'>C</div>)
+        coinArray.push(<div className='coin'>
+          <div className='coin--computer' />
+          <div className='coin--computer-border' />
+        </div>)
       } else if (value === User.HUMAN) {
-        coinArray.push(<div className='card card--human'>H</div>)
+        coinArray.push(<div className='coin'>
+          <div className='coin--human' />
+          <div className='coin--human-border' />
+        </div>)
       } else {
-        coinArray.push(<div className='card' />)
+        coinArray.push(<div className='coin' />)
       }
     }
     return coinArray;
