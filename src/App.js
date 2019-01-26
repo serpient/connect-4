ae9fc4053bfc3f3d9297e652576de0fc5137b663
serpient <littlegangwolf@gmail.com>
2019-01-25 20:30:47 -0800

    generateBoard map fn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+const generateBoard = () => {
+  let boardMap = new Map();
+  for (var i = 1; i < 7; i++) {
+    for (var j = 1; j < 8; j++) {
+      boardMap.set([i,j], null);
+    }
+  }
+  return boardMap;
+}
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: generateBoard(),
+    }
+  }
   render() {
+    console.log(this.state.board)
     return (
       <div className="App">
 
