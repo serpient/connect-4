82afb86b62814417d71abd78dfa39a09f240f9b8
serpient <littlegangwolf@gmail.com>
2019-01-27 14:27:58 -0800

    commented test units

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,17 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// with every turn, the current player keeps track of who goes next
+
+// when clicking a spot, a coin will drop into the most bottom row of that column
+
+// check the number of turns and how many rendered coins are on the board (should match)
+
+// clicking a column that is full will output message to click another empty column. but will not do anything
+
+// when checking for player coins, it accurately parses the right amount of empty, player 1, player 2
+
+// check for win (horizontal, diagonal, vertical)
+
+// check for no possible spaces left (tie)
