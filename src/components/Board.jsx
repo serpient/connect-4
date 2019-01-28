74ad1e1ff7ca8cc489ca26190ede8832e79b7ae2
serpient <littlegangwolf@gmail.com>
2019-01-27 17:22:20 -0800

    convert rendering to regular array

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,25 +2,38 @@ import React from 'react';
 import User from '../UserEnum';
 import './Board.scss';
 
-const Board = ({ boardData }) => {
-  const renderBoardSpaces = (mapObject) => {
-    let coinArray = [];
-    for (var value of mapObject.values()) {
-      if (value === User.COMPUTER) {
-        coinArray.push(<div className='coin'>
-          <div className='coin--computer' />
-          <div className='coin--computer-border' />
-        </div>)
-      } else if (value === User.HUMAN) {
-        coinArray.push(<div className='coin'>
-          <div className='coin--human' />
-          <div className='coin--human-border' />
-        </div>)
-      } else {
-        coinArray.push(<div className='coin' />)
-      }
-    }
-    return coinArray;
+const Board = ({ boardData, addCoinToBoard}) => {
+  const renderBoardSpaces = (array) => {
+    return array.map((row, rowIdx) => {
+      return row.map((coinInColumn, columnIdx) => {
+        let coin = null;
+        if (coinInColumn === User.COMPUTER) {
+          coin = (
+            <>
+              <div className='coin--computer' />
+              <div className='coin--computer-border' />
+            </>
+          )
+        } else if (coinInColumn === User.HUMAN) {
+          coin = (
+            <>
+              <div className='coin--human' />
+              <div className='coin--human-border' />
+            </>
+          )
+        }
+        return (
+          <div 
+            className='coin' 
+            key={rowIdx + ',' + columnIdx} 
+            onClick={() => addCoinToBoard(rowIdx, columnIdx)}
+          >
+            <div className='coin-position'>{`[${rowIdx}, ${columnIdx}]`} </div>
+            {coin}
+          </div>
+        )
+      })
+    })
   }
 
   return (
